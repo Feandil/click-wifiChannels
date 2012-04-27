64e19606cb6a43d53f35e03b5763a38ecaea4458
Vincent Brillault <git@lerya.net>
2012-04-27 10:37:38 +0200

    Output the most probable state

--- parameters/markovchain.cpp
+++ parameters/markovchain.cpp
@@ -84,13 +84,19 @@ void
 ParamMarckovChain::finalize(const uint32_t manx_rand)
 {
   uint32_t   i, tmp;
+  long double sum, max = 0;
 
   for (i = 0; i < state_mod; ++i) {
     tmp = (i << 1);
+    sum = ((long double)states[tmp + 1]) + states[tmp];
+    if (sum > max) {
+      max = sum;
+      state = i;
+    }
     if (states[tmp + 1] == 0) {
       transitions[i] = 0;
     } else {
-      transitions[i] = (uint32_t) (((long double) states[tmp + 1]) / ((long double) states[tmp + 1] + states[tmp]) * ((long double) manx_rand));
+      transitions[i] = (uint32_t) (((long double) states[tmp + 1]) / sum * manx_rand);
     }
   }
 }
@@ -116,6 +122,7 @@ ParamMarckovChain::printBinary()
   if (output_f != NULL ) {
     output_f->close();
   }
+  std::cout << "Most probable state : 0x" << std::hex << state << std::endl;
 }
 
 void
@@ -124,6 +131,7 @@ ParamMarckovChain::printHuman(const uint32_t max_rand)
   uint32_t temp;
   std::cout << "(MaxRand: 0x" << std::hex << max_rand << ")" << std::endl;
   std::cout << "State Number : 0x" << std::hex << max_rand << std::endl;
+  std::cout << "Most probable state : 0x" << std::hex << state << std::endl;
   std::cout << "Probability of success of transmission in state:" << std::endl;
   for (temp = 0; temp < state_mod; ++temp) {
     std::cout << "- 0x" << std::hex << temp << ": 0x%" << std::hex << transitions[temp] << " (" << ((long double)transitions[temp]/((long double) max_rand))*100 << "%)" << std::endl;
