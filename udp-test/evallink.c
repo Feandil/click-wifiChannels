260575da825f1d9104168d709e11b67cc16dfaba
Vincent Brillault <git@lerya.net>
2012-07-16 16:45:28 +0200

    udp-test: Fix segfault for daemon/slave version of evallink

--- udp-test/evallink.c
+++ udp-test/evallink.c
@@ -189,7 +189,9 @@ send_cb(struct ev_loop *loop, ev_periodic *periodic, int revents)
     return;
   }
   assert(sent_len == len);
-  update_time();
+  if (!(static_flags & EVALLINK_FLAG_DAEMON)) {
+    update_time();
+  }
 }
 
 static struct ev_periodic*
@@ -309,48 +311,69 @@ consume_data(struct timespec *stamp, uint8_t rate, int8_t signal, const struct i
         inc[pos].data.rate = rate;
         inc[pos].data.db = signal;
         memcpy(&inc[pos].data.stamp, stamp, sizeof(struct timespec));
+        if (!(static_flags & EVALLINK_FLAG_DAEMON)) {
+          size = snprintf(tmp, TMP_BUF, "%"PRIi8"dBm", signal);
+          assert(size > 0);
+          NCURSES_REWRITE_WINDOW_CONTENT(inc[pos].output.db, "%s", tmp);
+          size = snprintf(tmp, TMP_BUF, "%"PRIu8"%sMb/s", rate / 2, (rate % 2) ? ".5" : "");
+          assert(size > 0);
+          NCURSES_REWRITE_WINDOW_CONTENT(inc[pos].output.rate, "%s",tmp);
+        }
+        break;
+      }
+    } else {
+      memcpy(&inc[pos].data.ip, from, sizeof(struct in6_addr));
+      inc[pos].data.rate = rate;
+      inc[pos].data.db = signal;
+      memcpy(&inc[pos].data.stamp, stamp, sizeof(struct timespec));
+      if (!(static_flags & EVALLINK_FLAG_DAEMON)) {
+        ret = inet_ntop(AF_INET6, from, tmp, TMP_BUF);
+        assert(ret != NULL);
+        NCURSES_REWRITE_WINDOW_CONTENT(inc[pos].output.ip, "%s", tmp)
         size = snprintf(tmp, TMP_BUF, "%"PRIi8"dBm", signal);
         assert(size > 0);
         NCURSES_REWRITE_WINDOW_CONTENT(inc[pos].output.db, "%s", tmp);
         size = snprintf(tmp, TMP_BUF, "%"PRIu8"%sMb/s", rate / 2, (rate % 2) ? ".5" : "");
         assert(size > 0);
-        NCURSES_REWRITE_WINDOW_CONTENT(inc[pos].output.rate, "%s",tmp);
-        break;
+        NCURSES_REWRITE_WINDOW_CONTENT(inc[pos].output.rate, "%s", tmp);
       }
-    } else {
-      memcpy(&inc[pos].data.ip, from, sizeof(struct in6_addr));
-      ret = inet_ntop(AF_INET6, from, tmp, TMP_BUF);
-      assert(ret != NULL);
-      NCURSES_REWRITE_WINDOW_CONTENT(inc[pos].output.ip, "%s", tmp)
-      inc[pos].data.rate = rate;
-      inc[pos].data.db = signal;
-      memcpy(&inc[pos].data.stamp, stamp, sizeof(struct timespec));
-      size = snprintf(tmp, TMP_BUF, "%"PRIi8"dBm", signal);
-      assert(size > 0);
-      NCURSES_REWRITE_WINDOW_CONTENT(inc[pos].output.db, "%s", tmp);
-      size = snprintf(tmp, TMP_BUF, "%"PRIu8"%sMb/s", rate / 2, (rate % 2) ? ".5" : "");
-      assert(size > 0);
-      NCURSES_REWRITE_WINDOW_CONTENT(inc[pos].output.rate, "%s", tmp);
       break;
     }
   }
 
-  /* Then try to see if our ipaddress is inside thoses */
-  for (addr_pos = 0; addr_pos < MAX_ADDR; ++addr_pos) {
-    if (IN6_IS_ADDR_LINKLOCAL(&mon->ip_addr[addr_pos])) {
-      if (title == NULL) {
-        title =  newwin(LINE_HEIGHT, TITLE_LEN, TITLE_LINE, TITLE_COL);
-        ret = inet_ntop(AF_INET6, &mon->ip_addr[addr_pos], tmp, TMP_BUF);
-        assert(ret != NULL);
-        NCURSES_REWRITE_WINDOW_CONTENT(title, "%s", tmp)
-        wrefresh(title);
-      }
-      incoming = (struct in_air*) data;
-      while (*incoming->ip.s6_addr32 != 0) {
-        if (memcmp(&incoming->ip, &mon->ip_addr[addr_pos], sizeof(struct in6_addr)) == 0) {
-          for (pos = 0; pos < LINE_NB; ++pos) {
-            if (*out[pos].data.ip.s6_addr32 != 0) {
-              if (memcmp(&inc[pos].data.ip, from, sizeof(struct in6_addr)) == 0) {
+  if (!(static_flags & EVALLINK_FLAG_DAEMON)) {
+    /* Then try to see if our ipaddress is inside thoses */
+    for (addr_pos = 0; addr_pos < MAX_ADDR; ++addr_pos) {
+      if (IN6_IS_ADDR_LINKLOCAL(&mon->ip_addr[addr_pos])) {
+        if (title == NULL) {
+          title =  newwin(LINE_HEIGHT, TITLE_LEN, TITLE_LINE, TITLE_COL);
+          ret = inet_ntop(AF_INET6, &mon->ip_addr[addr_pos], tmp, TMP_BUF);
+          assert(ret != NULL);
+          NCURSES_REWRITE_WINDOW_CONTENT(title, "%s", tmp)
+          wrefresh(title);
+        }
+        incoming = (struct in_air*) data;
+        while (*incoming->ip.s6_addr32 != 0) {
+          if (memcmp(&incoming->ip, &mon->ip_addr[addr_pos], sizeof(struct in6_addr)) == 0) {
+            for (pos = 0; pos < LINE_NB; ++pos) {
+              if (*out[pos].data.ip.s6_addr32 != 0) {
+                if (memcmp(&inc[pos].data.ip, from, sizeof(struct in6_addr)) == 0) {
+                  out[pos].data.rate = incoming->rate;
+                  out[pos].data.db = incoming->db;
+                  size = snprintf(tmp, TMP_BUF, "%"PRIi8"dBm", incoming->db);
+                  assert(size > 0);
+                  NCURSES_REWRITE_WINDOW_CONTENT(out[pos].output.db, "%s", tmp);
+                  size = snprintf(tmp, TMP_BUF, "%"PRIu8"%sMb/s", incoming->rate / 2, (incoming->rate % 2) ? ".5" : "");
+                  assert(size > 0);
+                  NCURSES_REWRITE_WINDOW_CONTENT(out[pos].output.rate, "%s", tmp);
+                  memcpy(&out[pos].data.stamp, &incoming->stamp, sizeof(struct timespec));
+                  break;
+                }
+              } else {
+                memcpy(&out[pos].data.ip, from, sizeof(struct in6_addr));
+                ret = inet_ntop(AF_INET6, &out[pos].data.ip, tmp, TMP_BUF);
+                assert(ret != NULL);
+                NCURSES_REWRITE_WINDOW_CONTENT(out[pos].output.ip, "%s", tmp)
                 out[pos].data.rate = incoming->rate;
                 out[pos].data.db = incoming->db;
                 size = snprintf(tmp, TMP_BUF, "%"PRIi8"dBm", incoming->db);
@@ -362,29 +385,14 @@ consume_data(struct timespec *stamp, uint8_t rate, int8_t signal, const struct i
                 memcpy(&out[pos].data.stamp, &incoming->stamp, sizeof(struct timespec));
                 break;
               }
-            } else {
-              memcpy(&out[pos].data.ip, from, sizeof(struct in6_addr));
-              ret = inet_ntop(AF_INET6, &out[pos].data.ip, tmp, TMP_BUF);
-              assert(ret != NULL);
-              NCURSES_REWRITE_WINDOW_CONTENT(out[pos].output.ip, "%s", tmp)
-              out[pos].data.rate = incoming->rate;
-              out[pos].data.db = incoming->db;
-              size = snprintf(tmp, TMP_BUF, "%"PRIi8"dBm", incoming->db);
-              assert(size > 0);
-              NCURSES_REWRITE_WINDOW_CONTENT(out[pos].output.db, "%s", tmp);
-              size = snprintf(tmp, TMP_BUF, "%"PRIu8"%sMb/s", incoming->rate / 2, (incoming->rate % 2) ? ".5" : "");
-              assert(size > 0);
-              NCURSES_REWRITE_WINDOW_CONTENT(out[pos].output.rate, "%s", tmp);
-              memcpy(&out[pos].data.stamp, &incoming->stamp, sizeof(struct timespec));
-              break;
             }
           }
+          ++incoming;
         }
-        ++incoming;
       }
     }
+    update_time();
   }
-  update_time();
 }
 
 static void
