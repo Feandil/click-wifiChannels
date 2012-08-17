b82bed9b2b036ae6ee7f7a97fbd498205cf4682d
Vincent Brillault <git@lerya.net>
2012-08-17 11:48:46 +0200

    udp-test/server: Documentation, coding style, replace malloc/memset by calloc

--- udp-test/server.c
+++ udp-test/server.c
@@ -21,36 +21,83 @@
 
 /** @file server.c Main system of the tool that store received packets for later statistical analysis */
 
-/* udp buffers */
+/**
+ * Buffer size for transforming a binary IPv6 into a string
+ */
 #define ADDR_BUF_SIZE  128
+
+/**
+ * Buffer size for putting frame information into a string
+ */
 #define HDR_SIZE        16
+
+/**
+ * Buffer size for transforming binary date to string
+ */
 #define TIME_SIZE      128
 
+/**
+ * Internal data to be passed to the callbacks
+ */
 struct server_buffer {
-  struct mon_io_t mon;
-  char mon_name[IF_NAMESIZE];
-  char addr_s[ADDR_BUF_SIZE];
-  char date[TIME_SIZE];
-  char header[HDR_SIZE];
-  struct zutil_write zdata;
-  char *filename;
-  int encode;
-  int filename_count;
+  struct mon_io_t mon;        //!< Monitor "opaque" structure for monitor_listen_on and read_and_parse_monito
+  char mon_name[IF_NAMESIZE]; //!< Name of the monitoring interface
+  char addr_s[ADDR_BUF_SIZE]; //!< Buffer for turning a binary IPv6 into a string
+  char date[TIME_SIZE];       //!< Buffer size for transforming binary date to string
+  char header[HDR_SIZE];      //!< Buffer putting frame information into a string
+  struct zutil_write zdata;   //!< Zutil opaque structure to encode information
+  char *filename;             //!< Name of the file currently used for writing the encoded stream
+  int encode;                 //!< Zlib encoding level
+  int filename_count;         //!< In case of rotated files, index of the current file
 };
 
-/* Event loop */
+// Libev related variables
+
+/**
+ * Event loop containing all the events.
+ */
 struct ev_loop      *event_loop;
+
+/**
+ * Special event used for graceful endings.
+ * Unrolles all the other events, thus ends a "ev_loop(event_loop, 0);" call.
+ */
 struct ev_timer   *event_killer;
+/**
+ * Event linked to a monitoring socket and storing the incoming traffic
+ */
 struct ev_io         *glisten;
+/**
+ * Event linked to a normal socket in order to drop receive packet (Block icmp errors)
+ */
 struct ev_io         *gdrop;
+/**
+ * Periodic event that rotates the output file
+ */
 struct ev_periodic   *greload;
 
+/**
+ * Callback of the event_killer event.
+ * Unrolles all the other events, thus ends a "ev_loop(event_loop, 0);" call.
+ * Standard prototype for libev's ev_timer callback.
+ * @param loop    Event loop which needs to be stopped.
+ * @param w       Event that tiggered this call, should be 'event_killer'.
+ * @param revents Causes of this call.
+ */
 static void
 event_end(struct ev_loop *loop, struct ev_timer *w, int revents)
 {
   ev_unloop(event_loop, EVUNLOOP_ALL);
 }
 
+/**
+ * Callback of the gdrop event.
+ * Just drop the packet
+ * Standard prototype for libev's ev_io callback.
+ * @param loop    Event loop on which this event is.
+ * @param io      Event that tiggered this call, should be 'recv_mess'.
+ * @param revents Causes of this call.
+ */
 static void
 drop_cb(struct ev_loop *loop, ev_io *io, int revents)
 {
@@ -59,6 +106,19 @@ drop_cb(struct ev_loop *loop, ev_io *io, int revents)
   PRINTF("DROP\n");
 }
 
+/**
+ * Data received callback.
+ * Callback of a read_and_parse_monitor (from monitor.h) call, called by read_cb.
+ * Store the received information using zutil
+ * @param stamp     Timestamp of the reception
+ * @param rate      Rate at which the packet was received, in 0.5Mb/s.
+ * @param signal    Signal at which the packet was received, in dBm.
+ * @param from      IPv6 address of the sender.
+ * @param data      Pointer to the memory zone containing the content of the packet.
+ * @param len       Length of this memory zone.
+ * @param machdr_fc Flags of the received frame (can contain for example the "Retry" flag).
+ * @param arg       Pointer that was passed to the read_and_parse_monitor invocation, must contain a mon_io_t structure.
+ */
 static void
 consume_data(struct timespec *stamp, uint8_t rate, int8_t signal, const struct in6_addr *from, \
              const char* data, size_t len, uint16_t machdr_fc, void* arg)
@@ -68,14 +128,16 @@ consume_data(struct timespec *stamp, uint8_t rate, int8_t signal, const struct i
   int tmp;
   struct server_buffer* in;
 
-
+  /* Retrieve the buffer */
   assert(arg != NULL);
   in = (struct server_buffer*) arg;
 
+  /* Retrieve and store the source address */
   addr = inet_ntop(AF_INET6, from, in->addr_s, ADDR_BUF_SIZE);
   assert(addr != NULL);
   zadd_data(&in->zdata, addr, strlen(addr));
 
+  /* If there is a retry flag, store it. Also store signal strength and recieve rate */
   if ((machdr_fc & 0x0800) == 0x0800) {
     tmp = snprintf(in->header, HDR_SIZE, ",R,%"PRIi8",%"PRIu8, signal, rate);
   } else {
@@ -84,6 +146,10 @@ consume_data(struct timespec *stamp, uint8_t rate, int8_t signal, const struct i
   assert (tmp > 0);
   zadd_data(&in->zdata, in->header, (size_t)tmp);
 
+  /*
+   * The packet contains data ended by a '|' and then noise.
+   * Extract/store the data and discard the noise.
+   */
   assert(len > 0);
   end = memchr(data, '|', len);
   if (end == NULL) {
@@ -92,11 +158,21 @@ consume_data(struct timespec *stamp, uint8_t rate, int8_t signal, const struct i
     assert(end > data);
     zadd_data(&in->zdata, data, (size_t)(end - data));
   }
+  /* Add our timestamp at the end */
   tmp = snprintf(in->date, TIME_SIZE, ",%ld.%09ld\n", stamp->tv_sec, stamp->tv_nsec);
   assert(tmp > 0);
   zadd_data(&in->zdata, in->date, (size_t)tmp);
 }
 
+/**
+ * Callback of the recv_mess event.
+ * Use read_and_parse_monitor from monitor.h to analyse the packet,
+ * Use subfunction consume_data to analyse the data
+ * Standard prototype for libev's ev_io callback.
+ * @param loop    Event loop on which this event is.
+ * @param io      Event that tiggered this call, should be 'recv_mess'.
+ * @param revents Causes of this call.
+ */
 static void
 read_cb(struct ev_loop *loop, ev_io *io, int revents)
 {
@@ -107,6 +183,19 @@ read_cb(struct ev_loop *loop, ev_io *io, int revents)
   read_and_parse_monitor(&in->mon, consume_data, in);
 }
 
+/**
+ * Create the libev io event that listen on the interface.
+ * Use read_cb as the callback for that event
+ * @param port          Port to bind on
+ * @param mon_interface Name of the monitoring interface
+ * @param phy_interface Index (WIPHY) of the underlying physical interface to monitor on
+ * @param wan_interface Name of the underlying interface to monitor on
+ * @param multicast     Address to bind on
+ * @param out           Already opened FILE for storing the data
+ * @param encode        Level of zlib encoding
+ * @param filename      Filename of opened file, used for rotating it
+ * @return started libev io event which listen for new packets
+ */
 static struct ev_io*
 listen_on(in_port_t port, const char* mon_interface, const uint32_t phy_interface, const char* wan_interface, \
           const struct in6_addr* multicast, FILE* out, int encode, char* filename)
@@ -116,14 +205,14 @@ listen_on(in_port_t port, const char* mon_interface, const uint32_t phy_interfac
   struct server_buffer* buffer;
   struct mon_io_t* mon;
 
-  /* Create buffer */
-  buffer = (struct server_buffer *)malloc(sizeof(struct server_buffer));
+  /* Create local buffer */
+  buffer = calloc(1, sizeof(struct server_buffer));
   if (buffer == NULL) {
     PRINTF("Unable to use malloc\n")
     return NULL;
   }
-  memset(buffer, 0, sizeof(struct server_buffer));
 
+  /* Initialize the monitoring structure */
   mon = monitor_listen_on(&buffer->mon, port, mon_interface, phy_interface, wan_interface, multicast, 1);
   if (mon == NULL) {
     PRINTF("Unable to listen on monitoring interface")
@@ -131,6 +220,7 @@ listen_on(in_port_t port, const char* mon_interface, const uint32_t phy_interfac
   }
   assert(mon == &buffer->mon);
 
+  /* Store various information */
   strncpy(buffer->mon_name, mon_interface, IF_NAMESIZE);
   buffer->filename = filename;
   buffer->encode = encode;
@@ -154,6 +244,12 @@ listen_on(in_port_t port, const char* mon_interface, const uint32_t phy_interfac
   return event;
 }
 
+/**
+ * Create the libev io event that listen on the real interface and drop every packet.
+ * @param port          Port to bind on
+ * @param mreq          Multicast address to bind on (with scope)
+ * @return started libev io event which listen for new packets
+ */
 static struct ev_io*
 drop_on(in_port_t port, struct ipv6_mreq *mreq)
 {
@@ -194,6 +290,15 @@ drop_on(in_port_t port, struct ipv6_mreq *mreq)
   return event;
 }
 
+/**
+ * Callback of the greload event.
+ * Change the active file for output.
+ * Can also be call directly but no threading protection !!!
+ * Standard prototype for libev's ev_periodic callback.
+ * @param loop     Event loop on which this event is.
+ * @param periodic Event that tiggered this call, should be 'send_mess'.
+ * @param revents  Causes of this call.
+ */
 static void
 reload_cb(struct ev_loop *loop, ev_periodic *periodic, int revents)
 {
@@ -202,8 +307,11 @@ reload_cb(struct ev_loop *loop, ev_periodic *periodic, int revents)
   FILE* dest;
   int tmp;
 
+  /* Retrieve local data */
   in = (struct server_buffer*) periodic->data;
   assert(in != NULL);
+
+  /* Basic verification */
   if(in->filename == NULL) {
     fprintf(stderr, "Unable to change the name of the output file: no filename specified");
     return;
@@ -213,6 +321,8 @@ reload_cb(struct ev_loop *loop, ev_periodic *periodic, int revents)
     fprintf(stderr, "Unable to change the name of the output file: count >= 1000\n");
     return;
   }
+
+  /* Prepare the next filename */
   len = strlen(in->filename);
   assert(len > 7);
   snprintf(in->filename + (len - 6), 7, "%03i.gz", in->filename_count);
@@ -224,7 +334,6 @@ reload_cb(struct ev_loop *loop, ev_periodic *periodic, int revents)
     return;
   }
 
-
   /* Swap zlib opened stream */
   zend_data(&in->zdata);
   memset(&in->zdata, 0, sizeof(struct zutil_write));
@@ -232,30 +341,55 @@ reload_cb(struct ev_loop *loop, ev_periodic *periodic, int revents)
   assert(tmp >= 0);
 }
 
-static void reload(int sig)
+/**
+ * Callback for signals 'SIGHUP'.
+ * Need to be fixed
+ * @param sig Signal received (should be SIGHUP).
+ */
+static void
+reload(int sig)
 {
   if (!(ev_is_active(greload) && ev_is_pending(greload))) {
+    //!TODO: reschedule the reloading event
     reload_cb(event_loop, greload, SIGHUP);
   } else {
     printf("Reload should be triggered now thus just wait\n");
   }
 }
 
-static void down(int sig)
+/**
+ * Callback for signals.
+ * Use the event_killer event to stop the event loop.
+ * @param sig Signal received.
+ */
+static void
+down(int sig)
 {
   ev_timer_set(event_killer, 0, 0);
   ev_timer_start(event_loop, event_killer);
 }
 
 /* Default Values */
+//! Default "file" output (not recommanded)
 #define SERVER_DEFAULT_FILE stdout
+//! Default listening port
 #define SERVER_DEFAULT_PORT 10101
+//! Default Zlib encoding level
 #define SERVER_DEFAULT_ENCODE 7
+//! Default multicast address to listen on
 #define SERVER_DEFAULT_MULTICAST "ff02::1"
+//! Default interface to listen on
 #define SERVER_DEFAULT_INTERFACE "wlan0"
+//! Disable data file rotating by default
 #define SERVER_DEFAULT_RELOAD 0
 
-static void usage(int err, char *name)
+/**
+ * Print a short howto and exit.
+ * @param err Execution code to return.
+ * @param name Name under which this program was called.
+ */
+static void
+usage(int err, char *name)
 {
   printf("%s: Listen on a given socket and store timestamped packet content\n", name);
   printf("Usage: %s [OPTIONS]\n", name);
@@ -264,6 +398,7 @@ static void usage(int err, char *name)
   printf(" -o, --ouput  <file>  Specify the output file (default: standard output)\n");
   printf(" -r, --rand           Randomize the output file by adding a random number\n");
   printf("     --reload <secs>  Change the output file every <secs> seconds (disabled if <secs> <= 0, disabled by default)\n");
+  printf("                       If enabled, also grant the user to manually rotate the file by sending a SIGHUP. Danger: no threading protection !");
   printf(" -l, --level  [0-9]   Specify the level of the output compression (default : %i)\n", SERVER_DEFAULT_ENCODE);
   printf(" -p, --port   <port>  Specify the port to listen on (default: %"PRIu16")\n", SERVER_DEFAULT_PORT);
   printf(" -b           <addr>  Specify the address used for multicast (default : %s)\n", SERVER_DEFAULT_MULTICAST);
@@ -272,6 +407,9 @@ static void usage(int err, char *name)
   exit(err);
 }
 
+/**
+ * Long options used by getopt_long; see 'usage' for more detail.
+ */
 static const struct option long_options[] = {
   {"help",              no_argument, 0,  'h' },
   {"rand",              no_argument, 0,  'r' },
@@ -282,7 +420,15 @@ static const struct option long_options[] = {
   {NULL,                          0, 0,   0  }
 };
 
-int main(int argc, char *argv[]) {
+/**
+ * Main function for the server tool.
+ * @param argc Argument Count
+ * @param argv Argument Vector
+ * @return Execution return code
+ */
+int
+main(int argc, char *argv[])
+{
   int opt;
   int randi = 0;
   int encode = SERVER_DEFAULT_ENCODE;
